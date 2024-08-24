c41f25c59fb84dda53e7c1a03102e230c2060439
Leila <britezneira.leila@gmail.com>
2024-08-24 20:09:48 +0200

    Done : Exercice 10 completed

--- Exercice10/index.js
+++ Exercice10/index.js
@@ -1,9 +1,14 @@
 //votre code ici
 
-function printNumbers (number) {
-    for( n in number){
-        
+function printNumbers(number) {
+    let result = "";
+    for (let i = 1; i <= number; i++) {
+        result += i;
+        if (i < number) {
+            result += " ";
+        }
     }
+    return result;
 }
 
 export default printNumbers
